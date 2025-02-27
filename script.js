c30ee1f7fbf7fb81d6a43b097ae3120f2e31dda0
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2025-02-27 15:42:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ this.age = age;
 }
 
 Person.prototype.greet = function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 }
 
 function Employee(name, age, jobTitle) {
